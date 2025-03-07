13d9ccea4256af75b66ad3dc21f77ef1667143b5
JakeOrona <jakeorona@gmail.com>
2025-03-07 00:45:31 -0600

    update default testDir

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./downloads/playwright/",
+  testDir: "./fileStorage/playwright/",
   timeout: 60000,
   testMatch: '**/*.spec.ts',
-  reporter: [["html", {outputFolder: "downloads/reports/"}]],
+  reporter: [["html", {outputFolder: "fileStorage/reports/"}]],
   use: {
     headless: false, // Run tests in headless mode
     viewport: { width: 1280, height: 720 },
